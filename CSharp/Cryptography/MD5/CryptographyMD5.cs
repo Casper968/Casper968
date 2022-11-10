5322f51f667fc91a39d59f9570451cf57565b1d0
Casper Liu <lamliu968@gmail.com>
2022-11-10 11:17:58 +0800

    debug by console application

--- CSharp/Cryptography/MD5/CryptographyMD5.cs
+++ CSharp/Cryptography/MD5/CryptographyMD5.cs
@@ -1,15 +1,16 @@
-using System.Text;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
-namespace CSharp.Cryptography.MD5
+namespace playgroundconsoleproject.Cryptography.MD5
 {
     public static class CryptographyMD5
     {
         public static string ConvertToMD5(this string source)
         {
+            var md5 = string.Empty;
             using (var cryptoMD5 = System.Security.Cryptography.MD5.Create())
             {
                 // convert source string to utf8 bytes
@@ -19,11 +20,9 @@ namespace CSharp.Cryptography.MD5
                 var hash = cryptoMD5.ComputeHash(bytes);
 
                 // obtain exca MD5
-                var md5 = BitConverter.ToString(hash).Replace("-", String.Empty).ToUpper();
+                md5 = BitConverter.ToString(hash).Replace("-", String.Empty).ToUpper();
             }
+            return md5;
         }
-
-        // var password = "unicorn";
-        // var encrypted = password.ConvertToMD5();
     }
 }

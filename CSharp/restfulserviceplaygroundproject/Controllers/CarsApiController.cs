eda9c160720ac2f7928aaf9a546cd61bce6921e8
Casper Liu <lamliu968@gmail.com>
2022-11-12 23:38:13 +0800

    add brand list and filter param add model list and filter param

--- CSharp/restfulserviceplaygroundproject/Controllers/CarsApiController.cs
+++ CSharp/restfulserviceplaygroundproject/Controllers/CarsApiController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using restfulserviceplaygroundproject.DatabaseContext;
+using restfulserviceplaygroundproject.Infrastructure;
 
 namespace restfulserviceplaygroundproject.Controllers
 {
@@ -10,6 +13,24 @@ namespace restfulserviceplaygroundproject.Controllers
     [Route("api/[controller]")]
     public class CarsApiController : ControllerBase
     {
-        
+        CarsDbContext _carDbContext;
+
+        public CarsApiController(
+            CarsDbContext carsDbContext)
+        {
+            this._carDbContext = carsDbContext;
+        }
+
+        [HttpGet("Brands")]
+        public async Task<Result> GetCarBrands(string? name)
+        {
+            return Result.Ok(this._carDbContext.GetCarBrands(name));
+        }
+
+        [HttpGet("Models")]
+        public async Task<Result> GetCarModels(string? brandName, string? name)
+        {
+            return Result.Ok(this._carDbContext.GetCarModels(brandName, name));
+        }
     }
 }
--- CSharp/restfulserviceplaygroundproject/DatabaseContext/CarsDbContext.cs
+++ CSharp/restfulserviceplaygroundproject/DatabaseContext/CarsDbContext.cs
@@ -22,5 +22,47 @@ namespace restfulserviceplaygroundproject.DatabaseContext
         }
         
         public DbSet<CarBrand>? WorldCarBrand { get; set; }
+
+        public DbSet<CarModel>? WorldCarModel { get; set; }
+
+        public async Task<List<CarBrand>> GetCarBrands(string? name)
+        {
+            var brandList = await this.WorldCarBrand.ToListAsync();
+            if (brandList != null && brandList.Count > 0)
+            {
+                if (name != null)
+                {
+                    brandList = brandList.Where(x => x.Name.IndexOf(name) > -1).ToList();
+                }
+            }
+
+            return brandList;
+        }
+        
+        public async Task<List<CarModel>> GetCarModels(string? brandName, string? name)
+        {
+            var modelList = await this.WorldCarModel.ToListAsync();
+            if (modelList != null && modelList.Count > 0)
+            {
+                int brandId = 0;
+                var targetBrand = this.WorldCarBrand.FirstOrDefault(x => x.Name == brandName);
+                if (targetBrand != null)
+                {
+                    brandId = targetBrand.ID;
+                }
+
+                if (brandId > 0)
+                {
+                    modelList = modelList.Where(x => x.BrandId == brandId).ToList();
+                }
+                
+                if (name != null)
+                {
+                    modelList = modelList.Where(x => x.Name.IndexOf(name) > -1).ToList();
+                }
+            }
+
+            return modelList;
+        }
     }
 }
